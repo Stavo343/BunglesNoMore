a54f470193ce88fb69980719b7eb7d3fa778aefa
Stephen Nierste <spnierste@aol.com>
2018-06-20 19:31:13 -0400

    initial commit

---
+++ BunglesNoMore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BunglesNoMore.MvcApplication" Language="C#" %>
---
+++ BunglesNoMore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BunglesNoMore.Startup))]
+namespace BunglesNoMore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
